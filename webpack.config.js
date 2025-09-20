957a15b3a9a9b0638a95f7745e84aa4b2f10565f
Anton <abiryukov@mbs.media>
2025-09-20 19:05:43 +0300

    правка конфига

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.join(__dirname, './src/index.tsx'),
   output: {
-    path: path.join(__dirname, '/build'),
-    filename: 'main.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, './public/index.html'),
-    }),
-  ],
   resolve: {
     alias: {
       '@*': path.resolve(__dirname, 'src/*'),
     },
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss'],
   },
-  devServer: {
-    port: 3000,
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './public/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:8].css',
+      chunkFilename: '[name].[contenthash:8].chunk.css',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -31,12 +34,63 @@ module.exports = {
         },
       },
       {
+        test: /\.module\.s[ac]ss$/i,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+                exportLocalsConvention: 'camelCase',
+              },
+              sourceMap: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /\.module\.s[ac]ss$/i,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.module\.css$/i,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+                exportLocalsConvention: 'camelCase',
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/i,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          'css-loader',
+        ],
       },
       {
         test: /\.svg$/i,
@@ -44,5 +98,12 @@ module.exports = {
         use: ['@svgr/webpack'],
       },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
   },
 };
