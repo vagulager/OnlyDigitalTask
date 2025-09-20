ac19977ee702dc37b52c623f3f29cc6e310c765f
Anton <abiryukov@mbs.media>
2025-09-20 19:24:51 +0300

    правка конфига

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, './src/index.tsx'),
@@ -22,6 +23,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash:8].css',
       chunkFilename: '[name].[contenthash:8].chunk.css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/robots.txt',
+          to: 'robots.txt',
+        },
+      ],
     }),
   ],
   module: {
